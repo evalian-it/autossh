d2935d56d32b18b06c5e440866818f87eb86ada4
evalian-it <120847221+evalian-it@users.noreply.github.com>
2023-03-13 12:12:48 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Install Nessus
+curl --request GET \
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.4.2-debian9_amd64.deb' \
+  --output 'Nessus-10.4.1-debian9_amd64.deb'
+
+yes | dpkg -i Nessus-10.4.1-debian9_amd64.deb
+
+# Install auto-ssh & connection script
+touch /lib/systemd/system/c2autossh.service
+apt install autossh
+cat <<EOF > /lib/systemd/system/c2autossh.service
+[Unit]
+Description=AutoSSH tunnel to C2
+After=network.target
+[Service]
+Environment="AUTOSSH_GATETIME=0"
+User=kali
+Group=kali
+ExecStart=/usr/bin/autossh -M 11166 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "StrictHostKeyChecking no" -i /home/kali/.ssh/id_rsa -R 6667:localhost:22 vagrant@165.232.32.154
+RemainAfterExit=yes
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#chown kali:kali /home/kali/.ssh/id_rsa
+systemctl enable c2autossh
+systemctl start c2autossh
